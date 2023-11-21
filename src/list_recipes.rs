cfb7db1cd9a690f66e4e8ff67c77a04d79159145
Greg Shuflin <greg@everydayimshuflin.com>
2023-11-21 00:09:53 -0800

    Change how printing list works

--- src/list_recipes.rs
+++ src/list_recipes.rs
@@ -11,45 +11,62 @@ pub(crate) fn list_recipes_expanded(config: &Config, justfile: &Justfile) {
   );
 
   let doc_color = config.color.stdout().doc();
+  let list_prefix = config.list_prefix.as_str();
+  let print_doc = |doc: &str, name: &str| {
+    print!(
+      " {:padding$}{} {}",
+      "",
+      doc_color.paint("#"),
+      doc_color.paint(doc),
+      padding =
+        max_line_width.saturating_sub(line_widths.get(name).copied().unwrap_or(max_line_width))
+    );
+  };
+
   print!("{}", config.list_heading);
 
   for recipe in justfile.public_recipes(config.unsorted) {
     let name = recipe.name();
+    let aliases = recipe_aliases.get(name);
 
-    for (i, name) in iter::once(&name)
-      .chain(recipe_aliases.get(name).unwrap_or(&Vec::new()))
-      .enumerate()
-    {
-      print!("{}{name}", config.list_prefix);
-      for parameter in &recipe.parameters {
-        print!(" {}", parameter.color_display(config.color.stdout()));
-      }
-
-      // Declaring this outside of the nested loops will probably be more efficient,
-      // but it creates all sorts of lifetime issues with variables inside the loops.
-      // If this is inlined like the docs say, it shouldn't make any difference.
-      let print_doc = |doc| {
-        print!(
-          " {:padding$}{} {}",
-          "",
-          doc_color.paint("#"),
-          doc_color.paint(doc),
-          padding =
-            max_line_width.saturating_sub(line_widths.get(name).copied().unwrap_or(max_line_width))
-        );
-      };
-
-      match (i, recipe.doc) {
-        (0, Some(doc)) => print_doc(doc),
-        (0, None) => (),
-        _ => {
-          let alias_doc = format!("alias for `{}`", recipe.name);
-          print_doc(&alias_doc);
-        }
-      }
-      println!();
+    print_recipe(recipe, doc_color, &print_doc, list_prefix);
+    if let Some(aliases) = aliases {
+      print_aliases(aliases, list_prefix);
     }
   }
+}
+
+fn print_recipe(
+  recipe: &Recipe,
+  doc_color: Color,
+  print_doc: &dyn Fn(&str, &str),
+  list_prefix: &str,
+) {
+  let name = recipe.name();
+  print!("{}{name}", list_prefix);
+  for parameter in &recipe.parameters {
+    print!(" {}", parameter.color_display(doc_color));
+  }
+
+  if let Some(doc) = recipe.doc {
+    print_doc(doc, name);
+  }
+
+  println!();
+}
+
+fn print_aliases(aliases: &Vec<&str>, list_prefix: &str) {
+  let sigil = "╰─▶";
+  let descriptor = if aliases.len() >= 2 {
+    "aliases"
+  } else {
+    "alias"
+  };
+  print!("{list_prefix}{sigil} {descriptor}: ");
+  for item in aliases {
+    print!("{item} ");
+  }
+  println!("");
 }
 
 fn build_alias_table<'a>(justfile: &'a Justfile) -> BTreeMap<&'a str, Vec<&'a str>> {
